e2a119e2c9dc4d87e14505a5563bdf908a37266a
alivan48 <aalivanov44@gmail.com>
2026-05-27 21:38:10 +0900

    refactor: удалена AdminPage, очищен proposalsApi, обновлён App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import { HashRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import HomePage from "./pages/HomePage";
-import HolidayPage from "./pages/HolidayPage";
-import CalendarPage from "./pages/CalendarPage";
-import ContributePage from "./pages/ContributePage";
-import PeoplePage from "./pages/PeoplePage";
-import AdminPage from './pages/AdminPage';
-import "./App.css";
+import HomePage from './pages/HomePage';
+import HolidayPage from './pages/HolidayPage';
+import CalendarPage from './pages/CalendarPage';
+import ContributePage from './pages/ContributePage';
+import PeoplePage from './pages/PeoplePage';
+import './App.css';
 
 function App() {
   return (
@@ -14,12 +13,11 @@ function App() {
       <Navbar />
       <main className="app-main">
         <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/holiday/:id" element={<HolidayPage />} />
-          <Route path="/calendar" element={<CalendarPage />} />
+          <Route path="/"              element={<HomePage />} />
+          <Route path="/holiday/:id"   element={<HolidayPage />} />
+          <Route path="/calendar"      element={<CalendarPage />} />
           <Route path="/people/:people" element={<PeoplePage />} />
-          <Route path="/contribute" element={<ContributePage />} />
-          <Route path="/admin" element={<AdminPage />} />
+          <Route path="/contribute"    element={<ContributePage />} />
         </Routes>
       </main>
     </HashRouter>
